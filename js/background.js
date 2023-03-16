d66627a628b692c11a4058f7123733529c43edb7
McNorton <mcnorton@gmail.com>
2023-03-17 00:02:53 +0900

    Add. Background image selector

--- js/background.js
+++ js/background.js
@@ -1,53 +1,78 @@
 const images = [
-    "art.jpg",
+    "",
     "business.jpg",
     "clip.jpg",
-    "coffee.jpg",
     "desk.jpg",
-    "journey.jpg",
-    "paint.jpg",
+    "board.jpg",
+    "sky.jpg",
+    "rain.jpg",
+    "field.jpg",
 ];
 
 const bglist = document.getElementById("bglist");
 const background = document.body;
 const KEY_BGNAME = "background";
 
-const savedBGnumber = 1;
-// const savedBGnumber = localStorage.getItem(KEY_BGNAME);
-let backgroundImage;
+let savedBGname = localStorage.getItem(KEY_BGNAME);
+let bgImage;
 
-console.log(bglist);
-
-
-if (savedBGnumber === null) {
-    backgroundImage = images[Math.floor(Math.random()*images.length)];
+// 이미 저장된 배경화면이 있으면, 우선 보여줍니다.
+if (savedBGname === null || savedBGname == "") {
+    bgImage = images[Math.floor(Math.random()*(images.length-1)+1)];
 } else {
-    backgroundImage = images[Math.floor(savedBGnumber)];
+    bgImage = images[Math.floor(savedBGname)];
 }
 
+loadBG();
+
+// images[] 갯수만큼 * 표시 버튼을 생성합니다
 images.forEach(printBGbutton);
 
-function printBGbutton() {
+function printBGbutton(imgList) {
     const button = document.createElement("button");
-    button.innerText = "*";
-    button.classList.add("bgBtn");
-    button.addEventListener("click", changeBG);
+    const span = document.createElement("span");
+    const ionicon = document.createElement("ion-icon");
 
+    if (imgList == "") {
+        ionicon.setAttribute("name", "shuffle-outline");
+        button.classList.add("bgShuffle");
+        button.value = "";
+    } else {
+        ionicon.setAttribute("name", "ellipse");
+        button.classList.add("bgButton");
+        button.value = images.indexOf(imgList);
+    }
+
+    button.addEventListener("click", changeBG);
+    span.appendChild(ionicon);
+    button.appendChild(span);
     bglist.appendChild(button);
 }
 
+function loadBG() {
+    background.style.backgroundImage = `linear-gradient( rgba(0,0,0, 0.5), rgba(0,0,0, 0.5) ), url('img/${bgImage}')`;
+}
 
-function changeBG(event){
-    const btnDiv = [ ... event.target.parentElement.children];
-    const clkIndex = btnDiv.indexOf(event.target);
-    backgroundImage = images[Math.floor(clkButton.index)];
-    console.log(clkIndex);
-    saveBGnumber();
+function changeBG(event) {
+    const clickButton = event.target;
 
-    background.style.backgroundImage = `linear-gradient( rgba(0,0,0, 0.5), rgba(0,0,0, 0.5) ), url('img/${backgroundImage}')`;
+    if (clickButton.value == "") {
+        bgImage = images[Math.floor(Math.random()*(images.length-1)+1)];
+        removeBG();
+    } else {
+        bgImage = images[Math.floor(clickButton.value)];
+        savedBGname = clickButton.value;
+        saveBG();
+    }
+
+    loadBG();
 }
 
 
-function saveBGnumber() {
-    localStorage.setItem(KEY_BGNAME, savedBGnumber);
+function saveBG() {
+    localStorage.setItem(KEY_BGNAME, savedBGname);
+}
+
+function removeBG() {
+    localStorage.removeItem(KEY_BGNAME);
 }
