51b4fab98753a4b0103d2a9b3b3dbf94f3985686
McNorton <mcnorton@gmail.com>
2023-07-16 11:46:23 +0900

    new. Analog Clock

---
+++ js/analog.js
@@ -0,0 +1,56 @@
+const clockbelt = document.getElementById("clockbelt");
+const digitalclock = document.getElementById("clock");
+const analogclock = document.getElementById("analogclock");
+
+const anhrs = document.querySelector(".anhrs");
+const anmin = document.querySelector(".anmin");
+const ansec = document.querySelector(".ansec");
+
+let anaclockID = null;
+digitalclock.addEventListener("click", openAnalogClock);
+analogclock.addEventListener("click", openDigitalClock);
+
+openDigitalClock();
+
+function analogClockDraw() {
+    const d = new Date();
+
+    const hr = d.getHours();
+    const mn = d.getMinutes();
+    const sc = d.getSeconds();
+    const ms = d.getMilliseconds().toFixed(2);
+
+    const dgH = (hr * (360 / 12)) + ((mn * (360 / 60)) / 12);
+    const dgM = (mn * (360 / 60)) + ((sc * (360 / 60)) / 60);
+    const dgS = (sc * (360 / 60)) + ((ms * (360 / 60)) / 1000);
+
+    anhrs.style.transform = `translate(-50%,-85%) rotate(${dgH}deg)`;
+    anmin.style.transform = `translate(-50%,-85%) rotate(${dgM}deg)`;
+    ansec.style.transform = `translate(-50%,-85%) rotate(${dgS}deg)`;
+}
+
+
+function analogClockFace() {
+    /* const cface = anface.cloneNode(true);
+    for (i = 0; i < 12; i ++) {
+        let dotface = analogclock.appendChild(cface);
+        dotface.style.transform = 'rotate(30deg)';
+        dotface.style.top = '90%';
+        dotface.style.left = `${i}%`;
+    }*/
+}
+
+
+function openAnalogClock() {
+    anaclockID = setInterval(analogClockDraw, 100);
+    analogClockDraw();
+    clockbelt.hidden = true;
+    analogclock.hidden = false;
+}
+
+function openDigitalClock() {
+    clearInterval(anaclockID);
+    anaclockID = null;
+    analogclock.hidden = true;
+    clockbelt.hidden = false;
+}
